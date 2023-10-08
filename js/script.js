8e3312f981128ef95631417b09fcf7d2903cf02a
ziconstr <76712091+ziconstr@users.noreply.github.com>
2023-10-08 09:33:52 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,66 +1,54 @@
-const squareEyesAPI = "https://api.noroff.dev/api/v1/square-eyes";
+import { message } from "./components/message.js";
 
-const getMovieText = document.querySelectorAll(".movieText");
+const resultsContainer = document.querySelector(".posters");
+const postersDrama = document.querySelector(".posters-drama");
+const postersKids = document.querySelector(".posters-kids");
+const postersMenu = document.querySelector(".posters-menu");
 
-async function getMovies() {
-  const response = await fetch(squareEyesAPI);
-  const result = await response.json();
-  return result;
-}
+const baseApiUrl = "https://api.noroff.dev/api/v1";
+const endPointUrl = "/square-eyes";
 
+const url = `${baseApiUrl}${endPointUrl}`;
 
+async function fetchMovies() {
 
-async function displayMovies() {
-  const movies = await getMovies();
-  const moviesContainer = document.getElementById("movies_container");
-  console.log(moviesContainer.outerHTML);
-
-  for (i = 0; i < movies.length; i++) {
-    const movie = movies[i];
-
-
-    if (movie.onSale){
-        const movieDiv = document.createElement("div");
-        movieDiv.classList.add("movie");
-
-        const image = document.createElement("img");
-        image.srt = movie.image;
-        image.alt = movie.description;
-
-        const movieText = document.createElement("p");
-        movieText.classList.add("container-movies");
-        movieText.innerHTML = `${movie.tittle} <span class="movieSale">${movie.price} </span>  $${movie.discountedPrice}`;
-
-        const button = document.createElement("a");
-        button.href = "#";
-        button.classList.add("button");
-        button.textContent = "Add to bag";
-
-        moviesContainer.appendChild(image);
-        moviesContainer.appendChild(movieText);
-        moviesContainer.appendChild(button);
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
         
+        resultsContainer.innerHTML = "";
 
+        for (let i = 0; i < json.length; i++) {
+            
+            if (json[i].genre === "Action") {
+                resultsContainer.innerHTML += `<a href="video-page-showing.html?id=${json[i].id}" class="card">
+                                                <div>
+                                                <img class="posters-image" src="${json[i].image}" alt="${json[i].title}"/>
+                                                </div>
+                                               </a>`;
+            }
+            if (json[i].genre === "Drama" || json[i].genre === "Horror") {
+                postersDrama.innerHTML += `<a href="video-page-showing.html?id=${json[i].id}" class="card">
+                                                <div>
+                                                <img class="posters-image" src="${json[i].image}" alt="${json[i].title}"/>
+                                                </div>
+                                           </a>`;
+            }
+            if (json[i].genre === "Kids" || json[i].genre === "Comedy") {
+                postersKids.innerHTML += `<a href="video-page-showing.html?id=${json[i].id}" class="card">
+                                                <div>
+                                                <img class="posters-image" src="${json[i].image}" alt="${json[i].title}"/>
+                                                </div>
+                                          </a>`;
+            } 
+        }
     }
-  }
-}
+    catch (error) {
+        postersMenu.innerHTML = message("error", error);
+        }
+    }
 
 
-displayMovies ();
+fetchMovies();
 
 
-  // for (let i = 0; i < result.length; i++) {
-  //   console.log(result[i]);
-  // }
-
-  // for (let i = 0; i < getMovieText.length; i++) {
-  //   let moviesOnSale = result[i].onSale;
-
-  //   getMovieText[i].innerHTML = ` ${result[i].tittle} $${result[i].price}`;
-
-  //   if (moviesOnSale === true) {
-  //     result[i].price = getMovieText[
-  //       i
-  //     ].innerHTML = `${result[i].tittle} <span class="moviesale">${result[i].price} </span> $${result}
-  //   } 
-  // }
